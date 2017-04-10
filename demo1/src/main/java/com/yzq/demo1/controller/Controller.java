d39582c9ad168a2a8f537c10173f977f86f05474
martinyuan <xingyu_yzq@163.com>
2017-04-10 16:48:14 +0800

    add README.md

--- demo1/src/main/java/com/yzq/demo1/controller/Controller.java
+++ demo1/src/main/java/com/yzq/demo1/controller/Controller.java
@@ -1,14 +1,26 @@
 package com.yzq.demo1.controller;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class Controller {
 
-	@RequestMapping("/hello")
-	public String index() {
-		return "Hello World";
+	@ApiOperation(value = "欢迎首页", notes = "根据输入返回提示信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "用户名", required = false, dataType = "String",paramType="query") })
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String index(String name) {
+		if (StringUtils.isEmpty(name)) {
+			return "Hello World";
+		} else {
+			return "Hello " + name;
+		}
 	}
 
 }
